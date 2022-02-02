73c6d514303a67cccd3acba0a2dc161f8aca44ea
Stanislav Khomyakov <sikhomyakov@gmail..com>
2022-02-03 00:53:11 +0500

    Task2.3 Final

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -4,31 +4,17 @@ fun main() {
     val price = 22_000
     val standardDiscount = 100
     val fivePercentDiscount = (price * 0.05).toInt()
-    val regularCustomer: Boolean = true
+    val regularCustomer = true
 
-    val total = discount(price, standardDiscount, fivePercentDiscount)
+    val total = if (price <= 1000) price else if (price in 1001..10_000) price - standardDiscount
+    else if (price > 10_000) price - fivePercentDiscount else price
 
     if (regularCustomer) {
         val regularCustomerDiscount = total * 0.01
-        val result = (total - regularCustomerDiscount).toInt()
-        println("Сумма вашего заказа со скидками: $result руб.")
+        val regularCustomerTotal = (total - regularCustomerDiscount).toInt()
+        println("Сумма вашего заказа со скидками: $regularCustomerTotal руб.")
     } else {
         println("Сумма вашего заказа со скидками: $total руб.")
     }
 
 }
-
-private fun discount(price: Int, standardDiscount: Int, fivePercentDiscount: Int): Int =
-
-    when {
-        price <= 1000 -> {
-            price
-        }
-        price in 1001..10_000 -> {
-            price - standardDiscount
-        }
-        price > 10_000 -> {
-            price - fivePercentDiscount
-        }
-        else -> price
-    }
