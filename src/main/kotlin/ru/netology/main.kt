ce36f157cc48d997bdc5e815e09a85a7e4db4d7d
Stanislav Khomyakov <sikhomyakov@gmail..com>
2022-02-17 01:25:26 +0500

    Task 2.3.1 Final

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -10,6 +10,5 @@ fun main() {
     val totalStandardDiscount = (if (amount > 10_000) price * 0.95
     else if (amount >= 1001 && price <= 10_000) price - 100 else price).toDouble()
     val total = if (regularCustomer) totalStandardDiscount * 0.99 else totalStandardDiscount
-
     println("Сумма вашего заказа со скидками: ${total.toInt()} руб. ${(total * 100).roundToInt() % 100} коп.")
 }
